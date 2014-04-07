c38e2a195b362e041232a6d1eb520b19e98e5e25
Shawn Harkness <shawnhark@gmail.com>
2014-04-06 21:23:45 -0400

    start work on words controller

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,3 +1,17 @@
 class WordsController < ApplicationController
 
+  def index
+    @words = Word.all
+  end
+
+  def new
+    @words = Word.new
+  end
+
+  private
+  def word_params
+    params.require(:word).permit!
+  end
+
+
 end
