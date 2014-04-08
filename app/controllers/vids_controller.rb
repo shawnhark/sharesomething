d1e4b504a51067e5c624403b6a1b0bd4b045c77e
Shawn Harkness <shawnhark@gmail.com>
2014-04-07 21:27:47 -0400

    Start vids controller

--- app/controllers/vids_controller.rb
+++ app/controllers/vids_controller.rb
@@ -1,3 +1,16 @@
 class VidsController < ApplicationController
 
+  def index
+    @vids = Vid.all
+  end
+
+  def new
+    @vids = Vid.new
+  end
+
+  private
+  def word_params
+    params.require(:vid).permit!
+  end
+
 end
