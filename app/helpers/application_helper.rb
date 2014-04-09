c391445b42d418498b6070405fe15c65d0b4995e
Shawn Harkness <shawnhark@gmail.com>
2014-04-08 21:29:24 -0400

    start new word posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def fix_url(url)
+    url.starts_with?('http') ? url :  "http://#{url}"
+  end
+
+  def display_datetime(dt)
+      dt = dt.in_time_zone(current_user.time_zone) if logged_in?
+      dt.strftime("%A, %b %d, %Y at %I:%M%P %Z")
+  end
+
+  def generate_slug
+    self.slug = self.name.gsub(/[^0-9a-zA-Z]/, "_").downcase
+  end  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140325010830) do
+ActiveRecord::Schema.define(version: 20140406100158) do
 
   create_table "news_posts", force: true do |t|
     t.string   "title"
