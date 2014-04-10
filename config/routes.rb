44d4421ac2ab6f85a256320baf2d32cafe2b25af
Shawn Harkness <shawnhark@gmail.com>
2014-04-09 21:28:40 -0400

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Sharesomething::Application.routes.draw do
   root to: 'pages#front'
 
   get 'register', to: 'users#register'
-  get 'news_posts', to: 'news_posts#index'
-  get 'words', to: 'words#index'
-  get 'vids', to: 'vids#index'
-  get 'pics', to: 'pics#index'
+
+  resources :words
+  resources :pics
+  resources :vids
 
 end
