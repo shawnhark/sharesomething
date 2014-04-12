4b1f609d58a32afce1f98c3d1080a693ad65977e
Shawn Harkness <shawnhark@gmail.com>
2014-04-12 05:35:12 -0400

    start testing

--- spec/controllers/vids_controller_spec.rb
+++ spec/controllers/vids_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe VidsController do
-
+  describe "GET show" do
+    it "sets @vids"
+    it "sets @categories for vids"
+    it "sets @votes for vids"
+  end
 end
--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe WordsController do
-
+  describe "GET show" do
+    it "sets @words"
+    it "sets @categories for words"
+    it "sets @votes for words"
+  end
 end
