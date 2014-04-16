8ebfb61d50f6662b17f5b5629a4d5c8bf7fff2b9
Shawn Harkness <shawnhark@gmail.com>
2014-04-15 21:38:08 -0400

    Change routes for categories

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,11 @@ Sharesomething::Application.routes.draw do
 
   get 'register', to: 'users#register'
   get 'home', to: 'posts#index'
+  get 'words', to: 'words#index'
+  get 'pics', to: 'pics#index'
+  get 'vids', to: 'vids#index'
 
   resources :posts
   resources :categories
-
-  resources :words
-  resources :pics
-  resources :vids
 
 end
