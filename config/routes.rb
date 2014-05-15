cabe7977bbbbaaf9890337003a9caa80e112b88c
Shawn Harkness <shawnhark@gmail.com>
2014-05-14 21:33:13 -0400

    User adds new post

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Sharesomething::Application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
+  get 'new_posts', to: 'posts#new'
 
   resources :posts, except:[:destroy] do
     resources :comments, only: [:create]
