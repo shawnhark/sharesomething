4f491dc5a31a89ce396d824051fbe4ae78715fc8
Shawn Harkness <shawnhark@gmail.com>
2014-05-20 21:09:37 -0400

    Simplify comment search results

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
-  belongs_to :post, foreign_key: :id
+  belongs_to :post
   belongs_to :creator, class_name: 'User', foreign_key: :user_id
 
   validates_presence_of :content
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
 belongs_to :category
 belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
-has_many :comments, foreign_key: 'post_id'
+has_many :comments
 
   def post_id
     comment.post_id
