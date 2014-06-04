38379e8b9a2985c3793c33f1683fd35211a3dc67
Shawn Harkness <shawnhark@gmail.com>
2014-06-03 21:25:23 -0400

    uodate comment order for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   include Simplevote
   belongs_to :category
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
-  has_many :comments
+  has_many :comments, -> { order("created_at DESC") }
 
   def post_id
     comment.post_id
